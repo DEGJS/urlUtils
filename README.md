9e8af251260395a2a2326cc218fc70acf1905e85
Aaron Ladage <aladage@degdigital.com>
2015-09-28 14:27:12 -0500

    Updated getUrlParameter; added queryStringToJson

--- README.md
+++ README.md
@@ -2,7 +2,8 @@
 Utility functions for working with the browser URL.
 
 ## Available Methods
-* **getUrlParameter:** Get a query string parameter from the URL by name.
+* **getUrlParameter:** Get a named query string parameter from window URL or a string.
+* **queryStringToJson:** Create an object from a query string or window URL.
 
 ## Revision History
 * **1.0.0:** First commit.
--- urlUtils.js
+++ urlUtils.js
@@ -1,8 +1,26 @@
 let urlUtils = {
 
-	getUrlParameter: function(name) {
-		var match = RegExp('[?&]' + name + '=([^&]*)').exec(window.location.search);
+	getUrlParameter: function(name, queryString = window.location.search) {
+		let match = RegExp('[?&]' + name + '=([^&]*)').exec(queryString);
 		return match && decodeURIComponent(match[1].replace(/\+/g, ' '));
+	},
+
+	queryStringToJson: function(url = window.location.search) {
+	    if (url === '')
+	        return '';
+	    let pairs = url.slice(1).split('&'),
+	    	result = {};
+	    for (var idx in pairs) {
+	        var pair = pairs[idx].split('=');
+	        if (!!pair[0]) {
+	        	let val = decodeURIComponent(pair[1] || '');
+	        	if (val.indexOf(',') !== -1) {
+	        		val = val.split(',');
+	        	}
+	            result[pair[0].toLowerCase()] = val;
+	        }
+	    }
+	    return result;
 	}
 
 }
