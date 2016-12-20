110e17b526cb70e47d79ac702aa2f796f98b128c
Aaron Ladage <aladage@degdigital.com>
2016-12-20 12:37:19 -0600

    rewrote module and export format

--- src/urlUtils.js
+++ src/urlUtils.js
@@ -1,33 +1,32 @@
 import objectUtils from "DEGJS/objectUtils";
 
-let urlUtils = {
+function getUrlParameter(name, queryString = window.location.search) {
+	let match = RegExp('[?&]' + name + '=([^&]*)').exec(queryString);
+	return match && decodeURIComponent(match[1].replace(/\+/g, ' '));
+};
 
-	getUrlParameter: function(name, queryString = window.location.search) {
-		let match = RegExp('[?&]' + name + '=([^&]*)').exec(queryString);
-		return match && decodeURIComponent(match[1].replace(/\+/g, ' '));
-	},
+function queryStringToJson(queryString = window.location.search, normalize = true) {
+    if (queryString === '')
+        return '';
+    let pairs = queryString.slice(1).split('&'),
+    	result = {};
+    for (var idx in pairs) {
+        var pair = pairs[idx].split('=');
+        if (!!pair[0]) {
+        	let val = decodeURIComponent(pair[1] || '');
+        	if (val.indexOf(',') !== -1) {
+        		val = val.split(',');
+        	}
+        	if (normalize === true) {
+        		val = objectUtils.normalizeValue(val);
+        	}
+			result[pair[0]] = val;
+        }
+    }
+    return result;
+};
 
-	queryStringToJson: function(queryString = window.location.search, normalize = true) {
-	    if (queryString === '')
-	        return '';
-	    let pairs = queryString.slice(1).split('&'),
-	    	result = {};
-	    for (var idx in pairs) {
-	        var pair = pairs[idx].split('=');
-	        if (!!pair[0]) {
-	        	let val = decodeURIComponent(pair[1] || '');
-	        	if (val.indexOf(',') !== -1) {
-	        		val = val.split(',');
-	        	}
-	        	if (normalize === true) {
-	        		val = objectUtils.normalizeValue(val);
-	        	}
-				result[pair[0]] = val;
-	        }
-	    }
-	    return result;
-	}
-
-}
-
-export default urlUtils;
+export {
+	getUrlParameter,
+	queryStringToJson
+};
